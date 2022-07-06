d3ca5d4ca8617a8d7d1bdef331aef2e20f3aa60f
Shallon Kobusinge <kobshallon041@gmail.com>
2022-07-06 23:31:18 +0200

    feat: tailwind

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[tw`flex flex-col`, styles.container]}>
+      <Text >Open up App.js to start working on your app!</Text>
+      <Text >Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
